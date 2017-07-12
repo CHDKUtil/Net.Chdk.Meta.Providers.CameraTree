be56b646f93e9c107a79092135073f4a46bdec15
Dmitry Shechtman <damnian@gmail.com>
2017-07-12 20:05:42 +0300

    Add solution

---
+++ src/Net.Chdk.Meta.Providers.CameraTree/CameraTreeProvider.cs
@@ -0,0 +1,27 @@
+﻿using Net.Chdk.Meta.Model.CameraTree;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Net.Chdk.Meta.Providers.CameraTree
+{
+    sealed class CameraTreeProvider : ICameraTreeProvider
+    {
+        private IEnumerable<IInnerCameraTreeProvider> InnerProviders;
+
+        public CameraTreeProvider(IEnumerable<IInnerCameraTreeProvider> innerProviders)
+        {
+            InnerProviders = innerProviders;
+        }
+
+        public IDictionary<string, TreePlatformData> GetCameraTree(string path)
+        {
+            var ext = Path.GetExtension(path);
+            var writer = InnerProviders.SingleOrDefault(w => w.Extension.Equals(ext, StringComparison.OrdinalIgnoreCase));
+            if (writer == null)
+                throw new InvalidOperationException($"Unknown camera writer extension: {ext}");
+            return writer.GetCameraTree(path);
+        }
+    }
+}
