1a10cd12bb0ca725e90af8cf0cacc8c57015a8ee
Dmitry Shechtman <damnian@gmail.com>
2017-07-13 00:13:14 +0300

    Inherit from SingleExtensionProvider<>

--- src/Net.Chdk.Meta.Providers.CameraTree/CameraTreeProvider.cs
+++ src/Net.Chdk.Meta.Providers.CameraTree/CameraTreeProvider.cs
@@ -1,27 +1,22 @@
 ﻿using Net.Chdk.Meta.Model.CameraTree;
 using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 
 namespace Net.Chdk.Meta.Providers.CameraTree
 {
-    sealed class CameraTreeProvider : ICameraTreeProvider
+    sealed class CameraTreeProvider : SingleExtensionProvider<IInnerCameraTreeProvider>, ICameraTreeProvider
     {
-        private IEnumerable<IInnerCameraTreeProvider> InnerProviders;
-
         public CameraTreeProvider(IEnumerable<IInnerCameraTreeProvider> innerProviders)
+            : base(innerProviders)
         {
-            InnerProviders = innerProviders;
         }
 
         public IDictionary<string, TreePlatformData> GetCameraTree(string path)
         {
-            var ext = Path.GetExtension(path);
-            var writer = InnerProviders.SingleOrDefault(w => w.Extension.Equals(ext, StringComparison.OrdinalIgnoreCase));
-            if (writer == null)
-                throw new InvalidOperationException($"Unknown camera writer extension: {ext}");
-            return writer.GetCameraTree(path);
+            var provider = GetInnerProvider(path, out string ext);
+            if (provider == null)
+                throw new InvalidOperationException($"Unknown camera tree extension: {ext}");
+            return provider.GetCameraTree(path);
         }
     }
 }
